a141c63d3fd4fcbce4abe2cc4f46dcb4d369a797
Oksana <oxana.vita91@mail.ru>
2023-06-18 13:08:37 +0300

    Задача 58

---
+++ Task 58/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+{
+    int rows1 = matrix1.GetLength(0);
+    int cols1 = matrix1.GetLength(1);
+    int rows2 = matrix2.GetLength(0);
+    int cols2 = matrix2.GetLength(1);
+
+    if (cols1 != rows2)
+    {
+        throw new ArgumentException("Количество столбцов первой матрицы должно совпадать с количеством строк второй матрицы.");
+    }
+
+    int[,] result = new int[rows1, cols2];
+
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < cols2; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < cols1; k++)
+            {
+                sum += matrix1[i, k] * matrix2[k, j];
+            }
+            result[i, j] = sum;
+        }
+    }
+    return result;
+}
+
+int[,] GenerateRandomMatrix(int rows, int cols)
+{
+    Random random = new Random();
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = random.Next(1, 10); // Генерация случайного целого числа от 1 до 9
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write($"{matrix[i, j],-4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+// System.Console.Write("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите количество столбцов: ");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[rows, cols];
+// FillArray(array);
+// PrintArray(array);
+int[,] matrix1 = GenerateRandomMatrix(2, 2);
+int[,] matrix2 = GenerateRandomMatrix(2, 2);
+
+int[,] result = MultiplyMatrices(matrix1, matrix2);
+
+PrintMatrix(matrix1);
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine();
+PrintMatrix(result);
